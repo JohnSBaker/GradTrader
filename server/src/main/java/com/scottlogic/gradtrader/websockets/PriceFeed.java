01cb44163204413e921c5d01d23ffe9d6892fc1d
John Baker <jbaker@scottlogic.com>
2016-06-13 18:23:22 +0100

    PriceFeed with path param for pair

--- server/src/main/java/com/scottlogic/gradtrader/websockets/PriceFeed.java
+++ server/src/main/java/com/scottlogic/gradtrader/websockets/PriceFeed.java
@@ -1,10 +1,13 @@
 package com.scottlogic.gradtrader.websockets;
 
 import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
+import javax.ws.rs.PathParam;
 import javax.xml.crypto.Data;
 
 import org.atmosphere.config.service.WebSocketHandlerService;
@@ -21,14 +24,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.scottlogic.gradtrader.price.IncrementalPriceGenerator;
 import com.scottlogic.gradtrader.price.PriceGenerator;
 
-@WebSocketHandlerService(path = "/api/ws/price/gbpusd/", broadcaster = SimpleBroadcaster.class)
+@WebSocketHandlerService(path = "/api/ws/price/{pair}/", broadcaster = SimpleBroadcaster.class)
 public class PriceFeed extends WebSocketHandlerAdapter {
 
     Logger logger = LoggerFactory.getLogger(PriceFeed.class);
 
     private final ObjectMapper mapper = new ObjectMapper();
+    
+    private static double priceStart = 0.0;
    
-	PriceGenerator pg; // or map of 1 per pair?
+	Map<String,PriceGenerator> priceGenerators = new LinkedHashMap<String, PriceGenerator>(); 
 	
 	public PriceFeed(){
 		super();
@@ -40,17 +45,20 @@ public class PriceFeed extends WebSocketHandlerAdapter {
     
     @Override
     public void onOpen(WebSocket webSocket) throws IOException {
-    	String pair = "gbpusd";
     	try{
-	    	logger.debug("webSocket: {}", webSocket);   
-	    	logger.debug("factory: {}", factory);  	
-	    	Broadcaster broadcaster = factory.lookup("/api/ws/price/gbpusd/", true); 
-	    	logger.debug("broadcaster: {}", broadcaster);	    	
+    		String pathInfo = webSocket.resource().getRequest().getPathInfo();
+    		String[] decodedPath = pathInfo.split("/");
+    		String pair = decodedPath[decodedPath.length - 1];
+    		
+	    	Broadcaster broadcaster = factory.lookup("/api/ws/price/" + pair +"/", true); 
 	        webSocket.resource().setBroadcaster(broadcaster);
-	        logger.debug("resource: {}", webSocket.resource().toString());
+	        
+	        PriceGenerator pg = priceGenerators.get(pair);
 	        if (pg==null){
 	        	logger.debug("Generate prices for " + pair);
-	        	pg = new IncrementalPriceGenerator(pair, 1.0, 2.0, 0.2, 0.02);
+	        	priceStart = priceStart + 1.0;
+	        	pg = new IncrementalPriceGenerator(pair, priceStart, priceStart + 1.0, 0.2, 0.02);
+	        	priceGenerators.put(pair, pg);
 	        	Future<Object> future = broadcaster.scheduleFixedBroadcast(pg, 0, 5, TimeUnit.SECONDS);
 	        } else {
 	        	logger.debug("Already generating prices for " + pair);	        	
