d10a49658aa514e5dea60068fe01d3b108425e32
Sam Perridge <sperridge@scottlogic.co.uk>
2016-07-26 09:01:45 +0100

    fix delta arrow positions (#108)

--- client/src/components/priceTicker/PriceTicker.js
+++ client/src/components/priceTicker/PriceTicker.js
@@ -6,7 +6,7 @@ import PositiveArrowIcon from 'svg/PositiveArrow.svg';
 
 const PriceTicker = ({ price, delta, stacked }) => {
   if (!price) {
-    return <div className="loading">Loading</div>;
+    return <div className="price-ticker-loading">Loading</div>;
   }
   const main = price.slice(-3, -1);
   const pip = price.slice(-1);
@@ -21,26 +21,29 @@ const PriceTicker = ({ price, delta, stacked }) => {
   } else if (delta === -1) {
     deltaIcon = <NegativeArrowIcon />;
   } else {
-    deltaIcon = <div className="delta"></div>;
+    deltaIcon = <div className="price-ticker-delta"></div>;
   }
 
   const className = classnames('price-ticker', {
     'price-ticker-stacked': stacked,
   });
+  const startClassName = classnames('price-ticker-start', {
+    'price-ticker-start-stacked': stacked,
+  });
 
   return (
     <div className={className}>
-      <div>
-        <div className="start">
-          {start}
-        </div>
-        <div className="main">
+      <div className={startClassName}>
+        {start}
+      </div>
+      <div className="price-ticker-main-pip-delta">
+        <div className="price-ticker-main">
           {main}
         </div>
-      </div>
-      <div className="pip-delta">
-        {deltaIcon}
-        <div className="pip">{pip}</div>
+        <div className="price-ticker-pip-delta">
+          {deltaIcon}
+          <div className="price-ticker-pip">{pip}</div>
+        </div>
       </div>
     </div>
   );
